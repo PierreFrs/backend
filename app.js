c146ccb9eca32d226040da0862d33e811207702c
Pierre <pierrefraisse90@gmail.com>
2023-04-08 15:16:14 +0200

    Route correction

--- app.js
+++ app.js
@@ -21,13 +21,13 @@ app.use((req, res, next) => {
 
 app.get('/api/products', (req, res, next) => {
     Product.find()
-    .then(products => res.status(200).json(products))
+    .then(products => res.status(200).json({ products }))
     .catch(error => res.status(400).json({ error }));
 });
 
 app.get('/api/products/:id', (req, res, next) => {
     Product.findOne({ _id: req.params.id })
-    .then(product => res.status(200).json(product))
+    .then(product => res.status(200).json({ product }))
     .catch(error => res.status(404).json({ error }));
 });
 
@@ -48,7 +48,7 @@ app.put('/api/products/:id', (req, res, next) => {
 });
 
 app.delete('/api/products/:id', (req, res, next) => {
-    Thing.deleteOne({ _id: req.params.id })
+    Product.deleteOne({ _id: req.params.id })
     .then(() => res.status(200).json({ message: 'Deleted!'}))
     .catch(error => res.status(400).json({ error }));
 });
